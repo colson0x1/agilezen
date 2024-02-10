944fd3ce1949a785742dd31330695a620ff72bf3
Colson <colsoncalifornication@gmail.com>
2024-02-10 19:53:38 +0545

    handle project creation and update the UI

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
     });
   }
 
-  console.log(projectsState);
+  // console.log(projectsState);
 
   let content;
 
@@ -57,7 +57,10 @@ function App() {
 
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
