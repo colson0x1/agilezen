55e3c3c889bf0bea6033db8182ab20b2d6e84970
Colson <colsoncalifornication@gmail.com>
2024-02-10 17:20:06 +0545

    style sidebar and button with tailwind css

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,9 +1,13 @@
 export default function ProjectSidebar() {
   return (
-    <aside>
-      <h2>Your Projects</h2>
+    <aside className='w-1/3 px-8 py-16 bg-blue-900 text-blue-50 md:w-72 rounded-r-xl'>
+      <h2 className='mb-8 font-bold uppercase md:text-xl text-blue-200'>
+        Your Projects
+      </h2>
       <div>
-        <button>+ Add Project</button>
+        <button className='px-4 py-2 text-xs md:text-base rounded-md bg-blue-700 text-blue-400 hover:bg-blue-600 hover:text-blue-100'>
+          + Add Project
+        </button>
       </div>
       <ul></ul>
     </aside>
